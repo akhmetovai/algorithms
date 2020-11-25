b2ab5221f3ed25bb40fafd4ce51aa4ad025f9337
akhmetovai <48546189+akhmetovai@users.noreply.github.com>
2020-11-26 02:07:37 +0300

    Add files via upload

---
+++ proba_tasks/a.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+if n > 0:
+
+  rus = input().split()
+  foreign = input().split()
+
+  for i in range(n-1):
+    print(rus[i] + ' ' + foreign[i], end=' ')
+  print(rus[n-1] + ' ' + foreign[n-1])
+
+else:
+  print()
